ea3bda7ab0ed6e78ea620d649850239d6c852a9c
RockBrentwood <RockBrentwood@gmail.com>
2021-08-30 23:50:26 -0500

    Upgrade of the DAS disassembler.
    Upgrade of the configuration file format to include support for user-defined addresses and configuration options - already contained in the local version of the disassembler.

--- 8051/csd4-archive/assem/das.c
+++ 8051/csd4-archive/assem/das.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdint.h>
@@ -35,55 +36,32 @@ typedef uint16_t word;
 // Everything else is aliased, somewhere, into one of these spaces.
 // Note also that the subspace idata[0x80-0xff] is not officially available on the 8051.
 
-char *SFRs[0x80] = {
-  "P0", "SP", "DPL", "DPH", 0, 0, 0, "PCON",
-  "TCON", "TMOD", "TL0", "TL1", "TH0", "TH1", 0, 0,
-  "P1", 0, 0, 0, 0, 0, 0, 0, "SCON", "SBUF", 0, 0, 0, 0, 0, 0,
-  "P2", 0, 0, 0, 0, 0, 0, 0, "IE", 0, 0, 0, 0, 0, 0, 0,
-  "P3", 0, 0, 0, 0, 0, 0, 0, "IP", 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, "T2CON", 0, "RCAP2L", "RCAP2H", "TL2", "TH2", 0, 0,
-  "PSW", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  "ACC", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  "B", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+struct {
+   char *Name; byte Key;
+} DTab[] = {
+// MCS-51 generic special function byte registers.
+   {"P0", 0x80}, {"SP", 0x81}, {"DPL", 0x82}, {"DPH", 0x83},
+   {"PCON", 0x87}, {"TCON", 0x88}, {"TMOD", 0x89}, {"TL0", 0x8a},
+   {"TL1", 0x8b}, {"TH0", 0x8c}, {"TH1", 0x8d}, {"P1", 0x90},
+   {"SCON", 0x98}, {"SBUF", 0x99}, {"P2", 0xa0}, {"IE", 0xa8},
+   {"P3", 0xb0}, {"IP", 0xb8}, {"PSW", 0xd0}, {"ACC", 0xe0}, {"B", 0xf0},
+// 8052-specific registers.
+   {"T2CON", 0xc8}, {"RCAP2L", 0xca}, {"RCAP2H", 0xcb}, {"TL2", 0xcc}, {"TH2", 0xcd}
+}, BTab[] = {
+// MCS-51 generic special function bit registers.
+   {"IT0", 0x88}, {"IE0", 0x89}, {"IT1", 0x8a}, {"IE1", 0x8b}, {"TR0", 0x8c}, {"TF0", 0x8d},
+   {"TR1", 0x8e}, {"TF1", 0x8f}, {"RI", 0x98}, {"TI", 0x99}, {"RB8", 0x9a}, {"TB8", 0x9b},
+   {"REN", 0x9c}, {"SM2", 0x9d}, {"SM1", 0x9e}, {"SM0", 0x9f}, {"EX0", 0xa8}, {"ET0", 0xa9},
+   {"EX1", 0xaa}, {"ET1", 0xab}, {"ES", 0xac}, {"EA", 0xaf}, {"PX0", 0xb8}, {"PT0", 0xb9},
+   {"PX1", 0xba}, {"PT1", 0xbb}, {"PS", 0xbc}, {"RXD", 0xb0}, {"TXD", 0xb1}, {"INT0", 0xb2},
+   {"INT1", 0xb3}, {"T0", 0xb4}, {"T1", 0xb5}, {"WR", 0xb6}, {"RD", 0xb7}, {"P", 0xd0},
+   {"OV", 0xd2}, {"RS0", 0xd3}, {"RS1", 0xd4}, {"F0", 0xd5}, {"AC", 0xd6}, {"CY", 0xd7},
+// 8052-specific registers.
+   {"T2", 0x90}, {"T2EX", 0x91}, {"ET2", 0xad}, {"PT2", 0xbd}, {"CP_RL2", 0xc8}, {"C_T2", 0xc9},
+   {"TR2", 0xca}, {"EXEN2", 0xcb}, {"TCLK", 0xcc}, {"RCLK", 0xcd}, {"EXF2", 0xce}, {"TF2", 0xcf}
 };
 
-char *Bits[0x80] = {
-   0, 0, 0, 0, 0, 0, 0, 0,
-   "IT0", "IE0", "IT1", "IE1", "TR0", "TF0", "TR1", "TF1",
-   "T2", "T2EX", 0, 0, 0, 0, 0, 0,
-   "RI", "TI", "RB8", "TB8", "REN", "SM2", "SM1", "SM0",
-   0, 0, 0, 0, 0, 0, 0, 0,
-   "EX0", "ET0", "EX1", "ET1", "ES", "ET2", 0, "EA",
-   "RXD", "TXD", "INT0", "INT1", "T0", "T1", "WR", "RD",
-   "PX0", "PT0", "PX1", "PT1", "PS", "PT2", 0, 0,
-   0, 0, 0, 0, 0, 0, 0, 0,
-   "CP_RL2", "C_T2", "TR2", "EXEN2", "TCLK", "RCLK", "EXF2", "TF2",
-   "P", 0, "OV", "RS0", "RS1", "F0", "AC", "CY", 0, 0, 0, 0, 0, 0, 0, 0,
-   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
-
-// (Addressing? 4: 0) | OpBytes
-// Addressing is already determined from Code[] (true if and only if any of %L,%P,%R appear).
-// OpBytes is already determined from Code[] (1 for a valid mnemonic + 1 for %D,%R,%P,%B,%I + 2 for %L,%W,%X).
-byte Mode[0x100] = {
- 1,  6,  7,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 7,  6,  7,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 7,  6,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 7,  6,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 6,  6,  2,  3,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 6,  6,  2,  3,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 6,  6,  2,  3,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 6,  6,  2,  9,  2,  3,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
- 6,  6,  2,  1,  1,  3,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
- 3,  6,  2,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 2,  6,  2,  1,  1,  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
- 2,  6,  2,  1,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
- 2,  6,  2,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 2,  6,  2,  1,  1,  7,  1,  1,  6,  6,  6,  6,  6,  6,  6,  6,
- 1,  6,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
- 1,  6,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1
-};
+const size_t Ds = sizeof DTab/sizeof DTab[0], Bs = sizeof BTab/sizeof BTab[0];
 
 char *Code[0x100] = {
 "nop",           "ajmp %P",  "ljmp %L",     "rr A",
@@ -154,6 +132,7 @@ char *Code[0x100] = {
 
 bool Generating;
 unsigned Line;
+bool Recursing = true, NoData = false;
 
 void Error(bool Fatal, char *Format, ...) {
    fprintf(stderr, "[%u] ", Line);
@@ -162,19 +141,48 @@ void Error(bool Fatal, char *Format, ...) {
    if (Fatal) exit(EXIT_FAILURE);
 }
 
-char *LookUp(byte Type, word Key) {
-   switch (Type) {
-      default:
-   // cdata address.
-      case 0: return NULL; // For now.
-   // ddata address.
-      case 1: return Key >= 0x80 && Key < 0x100 && SFRs[Key - 0x80] != NULL? SFRs[Key - 0x80]: NULL;
-   // bdata address.
-      case 2: return Key >= 0x80 && Key < 0x100 && Bits[Key - 0x80] != NULL? Bits[Key - 0x80]: NULL;
-   }
+void *Allocate(size_t N) {
+   void *X = malloc(N); if (X == NULL) Error(true, "Out of memory.");
+   return X;
 }
 
-byte Ref[0x4000], Hex[0x4000];
+// ALink threads the symbols by Name and Namespace.
+// BLink threads the symbols by Key and Namespace.
+// CLink threads the symbols separately for each Namespace.
+typedef struct Symbol *Symbol;
+struct Symbol {
+   char *Name; word Key; bool Defined;
+   Symbol ALink, BLink, CLink;
+};
+
+Symbol GetSym(byte Type, char *Name) {
+   static Symbol HTab[0x100];
+   byte H = Type;
+   for (char *S = Name; *S != '\0'; H += *S++);
+   Symbol *HP = &HTab[H];
+   for (Symbol Sym = *HP; Sym != NULL; Sym = Sym->ALink) if (strcmp(Sym->Name, Name) == 0) return Sym;
+   Symbol Sym = Allocate(sizeof *Sym);
+   Sym->Name = strdup(Name), Sym->Defined = false, Sym->ALink = *HP;
+   return *HP = Sym;
+}
+
+char *LookUp(byte Type, Symbol Sym, word Key) {
+// Type = {0,1,2} for {c,d,b}data address.
+   static Symbol HTab[0x100];
+   byte H = Key ^ (Key >> 8) ^ Type;
+   Symbol *HP = &HTab[H], Cur, Prev;
+   for (Prev = NULL, Cur = *HP; Cur != NULL; Prev = Cur, Cur = Cur->BLink) if (Cur->Key == Key) {
+      if (Sym != NULL) {
+         Sym->Defined = true, Sym->Key = Key, Sym->BLink = Cur->BLink, Cur->BLink = NULL;
+         if (Prev == NULL) *HP = Sym; else Prev->BLink = Sym;
+      }
+      return Cur->Name;
+   }
+   if (Sym != NULL) Sym->Defined = true, Sym->Key = Key, Sym->BLink = *HP, *HP = Sym;
+   return NULL;
+}
+
+byte Ref[0x10000], Hex[0x10000];
 #define ENTRY 0x80
 #define OP    0x40
 #define ARG   0x20
@@ -188,14 +196,14 @@ const word *EEnd = Entries + sizeof Entries/sizeof Entries[0];
 
 void PushAddr(word Address) {
    if (!InRange(Address)) {
-      printf("External reference at %04x to %04x\n", PC, Address); return;
+      printf(";; External reference at %04x to %04x\n", PC, Address); return;
    } else if (Ref[Address]&ARG) {
       Error(false, "Entry into ARG at %04x.", PC); return;
    }
    if ((Ref[Address]&LINKS) <= LINKS) Ref[Address]++;
    if (Ref[Address]&ENTRY) return; else Ref[Address] |= ENTRY;
    if (Ref[Address]&OP) return;
-   if (EP >= EEnd) Error(true, "Too many entries, PC = %04x.", PC);
+   if (EP >= EEnd) Error(true, "Too many addresses, PC = %04x.", PC);
    *EP++ = Address;
 }
 
@@ -238,32 +246,32 @@ void HexLoad(void) {
       while (Ch != ':');
       byte CheckSum = 0;
       byte Size = GetByte(); word CurPC = GetWord(); bool Mark = GetByte() != 0;
-      byte Buffer[0x10]; for (word H = 0; H < Size; H++) Buffer[H] = GetByte();
+      byte HBuf[0x10]; for (word H = 0; H < Size; H++) HBuf[H] = GetByte();
       (void)GetByte();
       if (CheckSum != 0) Error(true, "Bad checksum.");
       if (Mark || Size == 0) break;
       if (CurPC < LoPC) LoPC = CurPC;
-      for (word H = 0; H < Size; H++, CurPC++) Hex[CurPC] = Buffer[H], Ref[CurPC] = 0;
+      for (word H = 0; H < Size; H++, CurPC++) Hex[CurPC] = HBuf[H], Ref[CurPC] = 0;
       if (CurPC > HiPC) HiPC = CurPC;
       if (getchar() == '\n') Line++;
    }
 }
 
-void PutDReg(byte D) { // This assumes the 8052.
-   char *Name = LookUp(1, D);
+void PutDReg(byte D) {
+   char *Name = LookUp(1, NULL, D);
    if (Name != NULL) printf("%s", Name); else PutByte(D);
 }
 
 void PutBReg(byte B) {
-   char *Name = LookUp(2, B);
+   char *Name = LookUp(2, NULL, B);
    if (Name != NULL) { printf("%s", Name); return; }
-   Name = LookUp(1, (B < 0x80? B >> 3 | 0x20: B&~7));
+   Name = LookUp(1, NULL, B < 0x80? B >> 3 | 0x20: B&~7);
    if (Name != NULL) { printf("%s.%u", Name, B&7); return; }
    PutByte(B);
 }
 
 void PutLabel(word C) {
-   char *Name = LookUp(0, C);
+   char *Name = LookUp(0, NULL, C);
    if (Name != NULL) { printf("%s", Name); return; }
    byte R = Ref[C];
    if (InRange(C)) printf("%c%04x", 'A' + (R&LINKS), C);
@@ -292,34 +300,31 @@ int Fetch(bool IsOp) {
          Ref[PC] |= ARG;
       }
    }
-   return Hex[PC++];
+   return (int)Hex[PC++];
 }
 
 void Disassemble(void) {
    for (int Ch; (Ch = Fetch(true)) != EOF; ) {
       byte Op = (byte)Ch;
-      byte Xs = Mode[Op];
-   // Indirectly-addressed jump or call.
+   // This is the only indirectly-addressed jump or call in the MCS-51.
       bool Indirect = Op == 0x73;
-   // For *jmp and ret* operations.
-      bool Breaking = (Op&0x1f) == 0x01 || Op == 0x80 || Op == 0x02 || (Op&~0x10) == 0x22;
+   // These are the *call operations.
+      bool Calling = (Op&0x1f) == 0x11 || Op == 0x12;
+   // These are the operations, *jmp and ret*, which do not go onto the operation addressed immediately after them.
+      bool Breaking = (Op&~0x10) == 0x22 || (Op&0x1f) == 0x01 || Op == 0x02 || Op == 0x80 || Op == 0x73;
       if (Generating) {
          if (!Breaking) printf("   ");
-      } else if (Indirect) printf("Indirect jump at %04x\n", PC - 1);
-   // True if and only if any of %L,%P,%R appear; i.e. the operation makes direct reference to an external code address.
-      bool Addressing = (Xs&4) != 0;
-   // 1 for a valid mnemonic + 1 for %D,%R,%P,%B,%I + 2 for %L,%W,%X.
-      char *Name = Code[Op];
-      for (char *S = Name; *S != '\0'; S++) {
+      } else if (Indirect) printf(";; Indirect jump at %04x\n", (unsigned)PC - 1);
+      for (char *S = Code[Op]; *S != '\0'; S++) {
          if (*S != '%') {
             if (Generating) putchar(*S);
             continue;
          }
-         byte B; word Lab;
+         byte Arg; word Lab;
          switch (*++S) {
          // 8-bit data or address:
             case 'B': case 'D': case 'I':
-               if ((Ch = Fetch(false)) == EOF) return; else B = (byte)Ch;
+               if ((Ch = Fetch(false)) == EOF) return; else Arg = (byte)Ch;
             break;
          // Relative address (8-bit signed):
             case 'R':
@@ -329,28 +334,28 @@ void Disassemble(void) {
             case 'P':
                if ((Ch = Fetch(false)) == EOF) return; else Lab = PC&0xf800 | (Op&0xe0) << 3 | (byte)Ch;
             break;
-         // 16-bit data or address
+         // 16-bit data or address:
             case 'L': case 'W': case 'X':
                if ((Ch = Fetch(false)) == EOF) return; else Lab = (byte)Ch;
                if ((Ch = Fetch(false)) == EOF) return; else Lab = Lab << 8 | (byte)Ch;
             break;
          }
          if (Generating) switch (*S) {
-            case 'I': putchar('#'), PutByte(B); break;
-            case 'B': PutBReg(B); break;
-            case 'D': PutDReg(B); break;
+            case 'I': putchar('#'), PutByte(Arg); break;
+            case 'B': PutBReg(Arg); break;
+            case 'D': PutDReg(Arg); break;
             case 'X': PutDReg(Lab&0xff), printf(", "), PutDReg(Lab >> 8); break;
             case 'R': case 'P': case 'L': PutLabel(Lab); break;
             case 'W': putchar('#'), PutWord(Lab); break;
             case 'i': printf("@R%1x", (unsigned)Op&1); break;
             case 'n': printf("R%1x", (unsigned)Op&7); break;
-            default: Error(true, "Bad format string, PC = %04x.", PC);
+            default: Error(true, "Bad format string, PC = %04x.", (unsigned)PC);
          } else switch (*S) {
             case 'R': case 'P': case 'L': PushAddr(Lab); break;
          }
       }
       if (Generating) putchar('\n');
-      if (Generating? Ref[PC] == 0: Breaking) break;
+      if (Breaking) break;
    }
 }
 
@@ -369,18 +374,100 @@ void PutData(byte *Buf, size_t N, size_t Max) {
    putchar('|'); putchar('\n');
 }
 
+Symbol BList = NULL, CList = NULL, DList = NULL;
 
 bool Configure(char *Path) {
+   char NBuf[0x20], *NEnd = NBuf + sizeof NBuf/sizeof NBuf[0] - 1;
+   for (int D = 0; D < Ds; D++) LookUp(1, GetSym(1, DTab[D].Name), DTab[D].Key);
+   for (int B = 0; B < Bs; B++) LookUp(2, GetSym(2, BTab[B].Name), BTab[B].Key);
    EP = Entries, Line = 1;
-   FILE *EntryF = fopen("entries", "r");
-   if (EntryF == NULL) return false;
-   for (int Ch; (Ch = fgetc(EntryF)) != EOF && isxdigit(Ch); ) {
-      word W = Nib(Ch);
-      while ((Ch = fgetc(EntryF)) != EOF && isxdigit(Ch)) W = W << 4 | Nib(Ch);
-      PushAddr(W);
-      if ((Ch = fgetc(EntryF)) == '\n') Line++; else ungetc(Ch, EntryF);
+   FILE *InF = fopen("entries", "r");
+   if (InF == NULL) return false;
+   int Ch = fgetc(InF);
+   char *S; Symbol Sym; int Type; word Key; bool Empty;
+StartLine:
+   while (Ch == ' ' || Ch == '\t') Ch = fgetc(InF);
+   switch (Ch) {
+      case EOF: fclose(InF); return true;
+      case 'B': case 'C': case 'D': case 'E':
+      case 'b': case 'c': case 'd': case 'e': Type = tolower(Ch); goto InList;
+      case 'X': case 'x': Recursing = false; goto EndLine;
+      case 'Y': case 'y': NoData = true; goto EndLine;
+      default:
+         if (isxdigit(Ch)) { Type = 'e'; goto EntryList; }
+      goto EndLine;
    }
-   return true;
+InList:
+   switch (Ch = fgetc(InF)) {
+      case EOF: case '\n': case '/': case ';':
+         if (Type == 'e') Error(false, "No entry points listed on an entry points line.");
+         else Error(false, "No definitions listed on a definitions line.");
+      goto EndLine;
+      case '\t': case ' ':
+         Empty = true, Ch = fgetc(InF);
+         if (Type == 'e') goto EntryList;
+      goto Definition;
+      default: goto InList;
+   }
+EntryList:
+   switch (Ch) {
+      case EOF: case '\n': case '/': case ';':
+         if (Empty) Error(false, "No entry points listed on an entry points line.");
+      goto EndLine;
+      default:
+         if (!isxdigit(Ch)) Ch = fgetc(InF);
+         else {
+            Empty = false;
+            for (Key = 0; isxdigit(Ch); Ch = fgetc(InF)) Key = Key << 4 | Nib(Ch);
+            PushAddr(Key);
+         }
+      goto EntryList;
+   }
+Definition:
+   switch (Ch) {
+      case EOF: case '\n': case '/': case ';':
+         if (Empty) Error(false, "No definitions listed on a definitions line.");
+      goto EndLine;
+      default:
+         if (isalpha(Ch) || Ch == '_') goto GetName;
+         Ch = fgetc(InF);
+      goto Definition;
+   }
+GetName:
+   for (Empty = false, S = NBuf; isalnum(Ch) || Ch == '_'; Ch = fgetc(InF))
+      if (S < NEnd) *S++ = Ch;
+   *S = '\0';
+   switch (Type) {
+      case 'b': Sym = GetSym(2, NBuf); break;
+      case 'd': Sym = GetSym(1, NBuf); break;
+      default: Sym = GetSym(0, NBuf); break;
+   }
+Define:
+   switch (Ch) {
+      case EOF: case '\n': case '/': case ';':
+         Error(false, "No address listed for %s.", Sym->Name);
+      goto EndLine;
+      default:
+         if (isxdigit(Ch)) goto GetValue;
+         Ch = fgetc(InF);
+      goto Define;
+   }
+GetValue:
+   for (Key = 0; isxdigit(Ch); Ch = fgetc(InF)) Key = Key << 4 | Nib(Ch);
+   if (Sym->Defined) Error(false, "%s is already defined.", Sym->Name);
+   else {
+      Sym->Defined = true, Sym->Key = Key;
+      switch (Type) {
+         case 'b': LookUp(2, Sym, Key), Sym->CLink = BList, BList = Sym; break;
+         case 'd': LookUp(1, Sym, Key), Sym->CLink = DList, DList = Sym; break;
+         default: LookUp(0, Sym, Key), Sym->CLink = CList, CList = Sym; break;
+      }
+   }
+goto Definition;
+EndLine:
+   while (Ch != '\n' && Ch != EOF) Ch = fgetc(InF);
+   if (Ch == '\n') Line++, Ch = fgetc(InF);
+goto StartLine;
 }
 
 int main(void) {
@@ -390,6 +477,12 @@ int main(void) {
       Error(false, "No entry points listed, using 0x%04x as the starting address.", LoPC), PushAddr(LoPC);
    while (EP > Entries) PC = *--EP, Disassemble();
    Generating = true, fprintf(stderr, "Second pass\n");
+   for (Symbol Sym = BList; Sym != NULL; Sym = Sym->CLink)
+      if (Sym->Defined) printf("%s bit ", Sym->Name), PutByte(Sym->Key), putchar('\n');
+   for (Symbol Sym = DList; Sym != NULL; Sym = Sym->CLink)
+      if (Sym->Defined) printf("%s %s ", Sym->Name, Sym->Key >= 0x80? "sfr": "data"), PutByte(Sym->Key), putchar('\n');
+   for (Symbol Sym = CList; Sym != NULL; Sym = Sym->CLink)
+      if (Sym->Defined) printf(";; %s code ", Sym->Name), PutWord(Sym->Key), printf(" [%d]\n", (Ref[Sym->Key] - 1)&LINKS);
    if (Ref[0]) printf("org 0\n");
    for (PC = 0x00; PC < HiPC; ) {
       byte Buf[0x10]; size_t Max = sizeof Buf/sizeof Buf[0], N;
